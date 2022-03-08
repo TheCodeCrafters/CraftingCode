ced60a3e12ca556e22e8e1d38cf45b8f48b53ed8
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-03-08 12:35:53 +0100

    implement lua language provider to make the CI build

--- lang-lua/src/main/java/io/github/thecodecrafters/craftingcode/lua/LuaProvider.java
+++ lang-lua/src/main/java/io/github/thecodecrafters/craftingcode/lua/LuaProvider.java
@@ -1,10 +1,29 @@
 package io.github.thecodecrafters.craftingcode.lua;
 
 import io.github.thecodecrafters.craftingcode.langapi.LanguageProvider;
+import io.github.thecodecrafters.craftingcode.langapi.VirtualMachine;
 
 public class LuaProvider implements LanguageProvider {
 	@Override
 	public String getLanguageName() {
 		return "LUA";
+	}
+
+	@Override
+	public String getLanguageVersion() {
+		return "5.3";
+	}
+
+	@Override
+	public String getLanguageDescription() {
+		return  """
+				A powerful, efficient, lightweight, embeddable scripting language.
+				It supports procedural programming, object-oriented programming, functional programming, data-driven programming, and data description.
+				""";
+	}
+
+	@Override
+	public VirtualMachine createMachine() {
+		return null;
 	}
 }
